e9e56af6dc62dd68dd2155131f8898c226d37b1d
Anatolii <gurov.anatoliy@gmail.com>
2020-12-10 21:51:43 +0200

    Старт

---
+++ General.Repository/Commons/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Repository.Commons
+{
+    public abstract class GenericRepository<T, TKey> //: IGenericRepository<T, TKey> where T : class, new()
+    {
+        //DbContext context;
+        //DbSet<T> dbSet;
+        //public GenericRepository(DbContext context)
+        //{
+        //    this.context = context;
+        //    dbSet = context.Set<T>();
+        //}
+        //public void Create(T obj)
+        //{
+        //    dbSet.AddOrUpdate(obj);
+        //}
+
+        //public void Update(T obj)
+        //{
+        //    dbSet.AddOrUpdate(obj);
+        //}
+        //public void Delete(TKey id)
+        //{
+        //    T obj = Get(id);
+        //    dbSet.Remove(obj);
+        //}
+
+        //public virtual IQueryable<T> GetAll()
+        //{
+        //    return dbSet;
+        //}
+        //public virtual IQueryable<T> FindBy(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        //{
+        //    return dbSet.Where(predicate);
+        //}
+
+        //public virtual T Get(TKey id)
+        //{
+        //    return dbSet.Find(id);
+        //}
+
+        //public void Save()
+        //{
+        //    context.SaveChanges();
+        //}
+    }
+}
---
+++ General.Repository/Commons/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Repository.Commons
+{
+    public interface IGenericRepository<T, TKey> where T : class, new()
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+        T Get(TKey id);
+        void Create(T obj);
+        void Update(T obj);
+        void Delete(TKey id);
+        void Save();
+    }
+}
