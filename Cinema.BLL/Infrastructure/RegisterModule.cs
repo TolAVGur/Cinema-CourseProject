06c393d08da364875c090b78b8e50808420fb5c6
Anatolii <gurov.anatoliy@gmail.com>
2020-12-12 01:13:17 +0200

    Доработал Infrastructure RegisterModule

--- Cinema.BLL/Infrastructure/RegisterModule.cs
+++ Cinema.BLL/Infrastructure/RegisterModule.cs
@@ -3,12 +3,7 @@ using Cinema.BLL.Models;
 using Cinema.BLL.Services;
 using Cinema.DAL.DomainModels;
 using Cinema.Repository.Repositories;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cinema.BLL.Infrastructure
 {
@@ -17,11 +12,31 @@ namespace Cinema.BLL.Infrastructure
         protected override void Load(ContainerBuilder builder)
         {
             //base.Load(builder);
+            builder.RegisterType(typeof(CinemaContext)).As(typeof(DbContext));
+
+            // Films - Фильмы
+            builder.RegisterType(typeof(FilmService)).As(typeof(IGenericService<FilmDTO, int>));
+            builder.RegisterType(typeof(FilmRepository)).As(typeof(IGenericService<Film, int>));
 
             // Halls - Залы 
             builder.RegisterType(typeof(HallService)).As(typeof(IGenericService<HallDTO, int>));
             builder.RegisterType(typeof(HallRepository)).As(typeof(IGenericService<Hall, int>));
-            builder.RegisterType(typeof(CinemaContext)).As(typeof(DbContext));
+
+            // lines - Ряды в замах
+            builder.RegisterType(typeof(LineService)).As(typeof(IGenericService<LineDTO, int>));
+            builder.RegisterType(typeof(LineRepository)).As(typeof(IGenericService<line, int>));
+
+            // Seances - Сеансы
+            builder.RegisterType(typeof(SeanseService)).As(typeof(IGenericService<SeanceDTO, int>));
+            builder.RegisterType(typeof(SeanseRepository)).As(typeof(IGenericService<Seance, int>));
+
+            // Tickets - Билеты
+            builder.RegisterType(typeof(TicketService)).As(typeof(IGenericService<TicketDTO, int>));
+            builder.RegisterType(typeof(TicketRepository)).As(typeof(IGenericService<Ticket, int>));
+
+            // Visitors - Клиенты
+            builder.RegisterType(typeof(VisitorService)).As(typeof(IGenericService<VisitorDTO, int>));
+            builder.RegisterType(typeof(VisitorRepository)).As(typeof(IGenericService<Visitor, int>));
         }
     }
 }
