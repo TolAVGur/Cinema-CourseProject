1e164711b41b047c6592081d81699ddc8d42aa13
Anatolii <gurov.anatoliy@gmail.com>
2020-12-18 01:27:19 +0200

    Разместил контролы на SeanceWindow

--- WpfUI/ViewModels/SeancesVM.cs
+++ WpfUI/ViewModels/SeancesVM.cs
@@ -8,5 +8,26 @@ namespace WpfUI.ViewModels
 {
     public class SeancesVM : BaseVM
     {
+
+
+
+
+
+        #region -- Обработка таймера - отображение времени
+        private string _myDate;
+        public string MyDate
+        {
+            get { return _myDate; }
+            set { _myDate = value; OnProperty(); }
+        }
+        private void ShowDateTimeToday()
+        {
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => { MyDate = $"Сегодня: {DateTime.Today,-15:d} Время: {DateTime.Now:T}"; };
+            timer.Start();
+        }
+        #endregion
     }
 }
